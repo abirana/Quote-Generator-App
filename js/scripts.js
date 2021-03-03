cbdffc3e6e2e4b8b69bacd9dce11ed699f4a7988
Abi Rana <iamyourfriend.abi@gmail.com>
2021-03-03 18:19:26 +0545

    Updated WP API integration

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,33 @@
-$(document).ready(function() {
-    $.ajax( {
-        url: 'https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&per_page=1&callback=',
-        success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-        
-        var linktext = $('#quote-content p').text();
+function generateQuote() {
+  $.ajax( {
+      url: 'https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&per_page=1&_fields=author,id,content,title&callback=',
+      success: function(data) {
+      var post = data.shift(); // The data is an array of posts. Grab the first one.
+      $('#quote-title').text(post.title.rendered);
+      $('#quote-content').html(post.content.rendered);
+      
+      var linktext = $('#quote-content p').text();
+  
+      $('a#get-tweet').attr("href", "https://twitter.com/intent/tweet?hashtags=design_quotes&text="+linktext);
+  
+      // If the Source is available, use it. Otherwise hide it.
+      if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+          $('#quote-source').html('- ' + post.custom_meta.Source);
+      } else {
+          $('#quote-source').text();
+      }
+  },
+  cache: false
+  });
+}
 
-        $('a#get-tweet').attr("href", "https://twitter.com/intent/tweet?hashtags=design_quotes&amp;related=freecodecamp&amp;text="+linktext);
-
-          // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-            $('#quote-source').html('- ' + post.custom_meta.Source);
-        } else {
-            $('#quote-source').text();
-        }
-    },
-    cache: false
-    });
+$(document).ready(function(e) {
+  generateQuote();
 });
 
 $('#get-another-quote').on('click', function(e) {
     e.preventDefault();
-    $.ajax( {
-      url: 'https://quotesondesign.com/wp/v2/wp-json/posts/?orderby=rand&per_page=1&callback=',
-      success: function(data) {
-        var post = data.shift(); // The data is an array of posts. Grab the first one.
-        $('#quote-title').text(post.title);
-        $('#quote-content').html(post.content);
-
-        var linktext = $('#quote-content p').text();
-
-        $('a#get-tweet').attr("href", "https://twitter.com/intent/tweet?hashtags=design_quotes&amp;related=freecodecamp&amp;text="+linktext);
-
-        // If the Source is available, use it. Otherwise hide it.
-        if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
-          $('#quote-source').html('- ' + post.custom_meta.Source);
-        } else {
-          $('#quote-source').text();
-        }
-      },
-      cache: false
-    });
+    generateQuote();
  });
 
 document.onkeyup=function(e){
